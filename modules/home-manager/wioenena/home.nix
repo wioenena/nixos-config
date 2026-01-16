4b98ee5ce249c0d874a2ed2e6b41a9a491035ff8
wioenena <wioenena@gmail.com>
2026-01-16 17:15:53 +0300

    .

--- modules/home-manager/wioenena/home.nix
+++ modules/home-manager/wioenena/home.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }:
 
@@ -11,6 +12,16 @@
   home.homeDirectory = "/home/wioenena";
 
   home.stateVersion = "25.11";
+
+  home.pointerCursor = {
+    enable = true;
+    x11.enable = true;
+    gtk.enable = true;
+    hyprcursor.enable = true;
+    name = "macOS";
+    size = 24;
+    package = pkgs.apple-cursor;
+  }; # TODO: move to a separate module
 
   programs.home-manager.enable = true;
 }
