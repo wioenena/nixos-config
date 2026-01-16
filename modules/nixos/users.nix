7193bd5c1d51d9500d264cfc59071d5e94e6c50e
wioenena <wioenena@gmail.com>
2026-01-16 03:27:09 +0300

    .

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 {
+  users.mutableUsers = false;
   users.users.wioenena = {
     isNormalUser = true;
-    initialPassword = "nixos";
     extraGroups = [ "wheel" ];
+    initialHashedPassword = "$6$rCtRKK/unc.5Wp0S$.33avtuFOR0YErCosOtlduw1RvLrIMUeYQ4oOE/kJfyOGBJV7M7ljeSXG/.iKeFaCERwCh8L3sc9pom1sRGj21";
     description = "Barış Köprülü";
     createHome = true;
     shell = pkgs.fish;
