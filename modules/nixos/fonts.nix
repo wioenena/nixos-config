1576aaefed073107b776ded6bcc4340b32060614
wioenena <wioenena@gmail.com>
2026-02-12 08:21:34 +0300

    .

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 {
   fonts.enableDefaultPackages = true;
+  fonts.fontconfig.enable = true;
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
+    corefonts
     font-awesome
     inter
     jetbrains-mono
